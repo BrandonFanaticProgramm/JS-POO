199dba33ca13d8b122a8944ff2af085b545868d5
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-04-07 12:12:53 -0500

    Añadiendo la primera seccion del curso que sera objectos y sus propiedades

---
+++ Object/Object.js
@@ -0,0 +1,19 @@
+// Literal Object
+
+// Example
+
+const Dog = {
+    name : "Sally",
+    age : 10,
+    color: "white",
+    family : ["Brandon", "Lala","Charly","Cindy"],
+    vaccunes : {
+        rage : true,
+        fleas : true,
+        ticks : true,
+    }
+
+    //Properties in oop
+}
+
+console.log(Dog);
