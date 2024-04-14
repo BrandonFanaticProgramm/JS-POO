5bd4772720e03b43727c4a771f421735073b1422
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-04-14 16:17:58 -0500

    Añadiendo el concepto ---> Prototype

---
+++ Prototype/Prototype.js
@@ -0,0 +1,36 @@
+// PROTOTYPE
+
+// EXAMPLE 1
+
+function Persona(name,lastname) {
+
+    this.name = name;
+    this.lastname = lastname;
+    this.displayName = function()  {
+
+        return `${this.name} ${this.lastname}`;
+    }
+}
+const person1 = new Persona("Brandon","Quintero");
+const person2 = new Persona("Maria","Suarez");
+person1.salary = 23000;
+// person2.greeting = function() {
+//     console.log(`Hello I"m ${this.name}`);
+// }
+Persona.prototype.greeting = function () {
+
+    console.log("Hello I'm",this.name);
+} 
+console.log(person1);
+console.log(person2)
+person2.greeting();
+// person1.greeting();
+
+
+// EXAMPLE 2 --> MODIFY THE EXISTENT PROTOTYPE
+
+const MyName = new String("Brandon Alexis Quintero");
+String.prototype.value = function () {
+    return 1;
+};
+console.log(MyName.value());
