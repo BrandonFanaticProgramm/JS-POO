1b1e833c272a4743c696a33dc5c43de007703c52
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-04-15 21:54:36 -0500

    Nuevo Concepto ---> Agregacion

---
+++ Agregacion/Agregacion.js
@@ -0,0 +1,14 @@
+class Person {
+
+    constructor(name,age,id) {
+
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+}
+
+const Company = {
+    
+    name : "PROGRAMADORES TECH"
+}
