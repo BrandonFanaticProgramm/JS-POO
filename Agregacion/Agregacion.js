6a26bbe3bebf6799c62e0d1b08db6a10bd46a620
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2024-04-17 04:55:29 -0500

    Modificando y poniendo ejemplos de la agregacion en javascript

--- Agregacion/Agregacion.js
+++ Agregacion/Agregacion.js
@@ -1,3 +1,7 @@
+// AGREGATION
+
+// EXAMPLE 1 ADD INSTANCE OF CLASS TO A OBJECT
+
 class Person {
 
     constructor(name,age,id) {
@@ -9,6 +13,17 @@ class Person {
 }
 
 const Company = {
-    
-    name : "PROGRAMADORES TECH"
+    name : "PROGRAMADORES TECH",
+    employeers : [],
 }
+
+const user = new Person("Jay Bahamon",19,1015069872);
+const user2 = new Person("Ryan Marquez", 18,1015874526);
+
+Company.employeers.push(user);
+Company.employeers.push(user2);
+
+console.log(Company);
+
+
+
